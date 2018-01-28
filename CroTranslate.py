611044254f59be44bb183f88f130f61bc6839390
Luke Dobrovic <lukedobrovic@Luke-Dobrovics-MacBook.local>
2018-01-28 16:27:21 -0500

    Just a quick update with a new sentence

--- CroTranslate.py
+++ CroTranslate.py
@@ -1,6 +1,8 @@
 sentence1 = 'I went to the store and bought some cheese today.'
 sentence2 = 'I went to the store and bought some cheese hoorah.'
 sentence3 = 'Shijit says he owns a store'
+sentence4 = 'Luke is taller than Shijit'
+
 
 def translate(sent, english, croatian):
     new_sentence = ''
@@ -14,6 +16,7 @@ def translate(sent, english, croatian):
 print(translate(sentence1, 'cheese', 'sir'))
 print(translate(sentence2, 'some', 'malo'))
 print(translate(sentence3, 'says', 'kaže da') )
+print(translate(sentence4, 'taller', 'viši'))
 
         # shijit's hint: make a "new sentence" and throw words into new sentence by adding them and then wen you it the word
 # to translate, just throw in the translated word instead.
