4ce26a1eebe92e0cff5c4409e096d61b27407432
Luke Dobrovic <lukedobrovic@Luke-Dobrovics-MacBook.local>
2018-01-28 16:17:00 -0500

    This is an elementary English to Croatian translation "app"

---
+++ CroTranslate.py
@@ -0,0 +1,19 @@
+sentence1 = 'I went to the store and bought some cheese today.'
+sentence2 = 'I went to the store and bought some cheese hoorah.'
+sentence3 = 'Shijit says he owns a store'
+
+def translate(sent, english, croatian):
+    new_sentence = ''
+    for item in sent.split(' '):
+        if item == english:
+            new_sentence = new_sentence + croatian + ' '
+        else:
+            new_sentence = new_sentence + item + ' '
+    return new_sentence
+
+print(translate(sentence1, 'cheese', 'sir'))
+print(translate(sentence2, 'some', 'malo'))
+print(translate(sentence3, 'says', 'kaže da') )
+
+        # shijit's hint: make a "new sentence" and throw words into new sentence by adding them and then wen you it the word
+# to translate, just throw in the translated word instead.
